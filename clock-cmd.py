c5a8d1a201797656b4da3a83ee007eeb5c59ad90
NebulaDev69 <mikorichard8221@outlook.com>
2025-05-03 07:18:14 +0000

    Create clock-cmd.py

---
+++ clock-cmd.py
@@ -0,0 +1,9 @@
+import time
+
+def clock_once():
+    print("Nebula Clock v1.0")
+    print("------------------")
+    print("Current Time:", time.strftime("%H:%M:%S"))
+
+if __name__ == "__main__":
+    clock_once()
